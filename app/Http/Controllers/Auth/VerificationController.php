a73393544c21c6d30d863ac52bd82b0e132a25f0
amiradbrains <amir.adbrains@gmail.com>
2024-10-16 13:07:13 +0530

    verfication link redirect to profile page

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/upload-video/TNDS-S1';
 
     /**
      * Create a new controller instance.
